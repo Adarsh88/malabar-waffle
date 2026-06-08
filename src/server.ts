98acebcf4de9e1f4aa600a55ae98d5503b05b390
Adarsh Narayanan <adarsh.narayanan@sailygroup.com>
2026-06-08 17:55:37 +0530

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,25 @@
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from "@angular/ssr/node";
-import express from "express";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
+import { AngularAppEngine, createRequestHandler } from "@angular/ssr";
+import { getContext } from "@netlify/angular-runtime/app-engine.js";
 
-const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, "../browser");
-const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularAppEngine = new AngularAppEngine();
 
-app.use(express.static(browserDistFolder, { maxAge: "1y", index: false }));
-app.use("/{*splat}", (req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
+export async function netlifyAppEngineHandler(
+  request: Request,
+): Promise<Response> {
+  const context = getContext();
 
-if (isMainModule(import.meta.url)) {
-  const port = process.env["PORT"] || 4000;
-  app.listen(port, () =>
-    console.log(`Node Express server listening on http://localhost:${port}`),
-  );
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  const result = await angularAppEngine.handle(request, context);
+  return result || new Response("Not found", { status: 404 });
 }
 
-export const reqHandler = createNodeRequestHandler(app);
+/**
+ * The request handler used by the Angular CLI (dev-server and during build).
+ */
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
